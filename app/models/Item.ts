35ce341622ec0c9f64a881f09163bc3f51def183
Joshua Gretton <jb.gretton@googlemail.com>
2025-08-25 19:00:07 +0100

    Updated Model
    Removed required: false as this is inferred by not including required. Added trim to name.

--- app/models/Item.ts
+++ app/models/Item.ts
@@ -2,11 +2,11 @@ import mongoose, { Schema } from "mongoose";
 import { Item, ITEM_TYPE } from "../types/mongoose";
 
 const ItemSchema = new Schema<Item>({
-	name: { type: String, required: true },
-	description: { type: String, required: false },
-	type: { type: String, enum: Object.values(ITEM_TYPE) },
-	cost: { type: Number, required: false },
-	weight: { type: Number, required: false },
+	name: { type: String, required: true, trim: true },
+	description: { type: String, required: true },
+	type: { type: String, enum: Object.values(ITEM_TYPE), required: true },
+	cost: Number,
+	weight: Number,
 });
 
 export default mongoose.models.Item || mongoose.model<Item>("Item", ItemSchema);
