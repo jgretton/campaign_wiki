4b0bdb3c86d748d57ac23806578d819bad592b8b
Joshua Gretton <jb.gretton@googlemail.com>
2025-08-25 19:01:09 +0100

    Character Model and Type Update
    Updated and addded types for Character. Implemented Character schema and  model.

---
+++ app/models/Character.ts
@@ -0,0 +1,87 @@
+import mongoose, { Schema } from "mongoose";
+import { Character, Stat, Stats, PROFESSION, STATUS } from "../types/mongoose";
+
+const StatSchema = new Schema<Stat>(
+	{
+		value: { type: Number, required: true },
+		modifier: { type: Number, required: true },
+		save: { type: Number, required: true },
+	},
+	{ _id: false }
+);
+
+const StatsSchema = new Schema<Stats>(
+	{
+		ac: { type: Number, required: true },
+		speed: { type: Number, required: true },
+		hp: { type: Number, required: true },
+		cr: { type: Number, required: true, min: 0, max: 30 },
+
+		con: StatSchema,
+		str: StatSchema,
+		dex: StatSchema,
+		int: StatSchema,
+		wis: StatSchema,
+		cha: StatSchema,
+
+		traits: {
+			type: [
+				{
+					title: { type: String, required: true },
+					description: { type: String, required: true },
+				},
+			],
+			default: [],
+		},
+		actions: {
+			type: [
+				{
+					title: { type: String, required: true },
+					description: { type: String, required: true },
+				},
+			],
+			default: [],
+		},
+	},
+	{ _id: false }
+);
+
+const CharacterSchema = new Schema<Character>({
+	public: {
+		name: { type: String, required: true, trim: true },
+		age: { type: Schema.Types.Mixed, required: true },
+		appearance: { type: String, required: true },
+		description: { type: String, required: true },
+		additional_notes: {
+			type: [
+				{
+					session: { type: String, required: true, trim: true },
+					discovery: { type: String, required: true },
+				},
+			],
+			default: [],
+		},
+		related_characters: { type: [String], default: [] },
+		languages: { type: [String], default: [] },
+		location: { type: String, default: "unknown", trim: true },
+		profession: {
+			type: String,
+			enum: Object.values(PROFESSION),
+			required: true,
+		},
+		status: {
+			type: String,
+			enum: Object.values(STATUS),
+			required: true,
+		},
+	},
+	dm: {
+		immunities: { type: [String], default: [] },
+		alignment: { type: String, required: true },
+		stats: { type: StatsSchema, required: true },
+	},
+	draft: { type: Boolean, default: true },
+});
+
+export default mongoose.models.Character ||
+	mongoose.model<Character>("Character", CharacterSchema);
--- app/types/mongoose/index.ts
+++ app/types/mongoose/index.ts
@@ -2,8 +2,8 @@ import { Document } from "mongoose";
 
 export interface Item extends Document {
 	name: string;
-	description?: string;
-	type?: ITEM_TYPE;
+	description: string;
+	type: ITEM_TYPE;
 	cost?: number;
 	weight?: number;
 }
@@ -12,4 +12,73 @@ export enum ITEM_TYPE {
 	Item = "Item",
 	Armour = "Armour",
 	Weapon = "Weapon",
+}
+
+export interface Character extends Document {
+	public: {
+		name: string;
+		age: string | number;
+		appearance: string;
+		description: string; // History and information about the character
+		additional_notes: Additional_Notes[]; // this is more for session update (eg. Session 3 - players find out X was part of the Watchers cult)
+		related_characters: string[]; //array of character ids
+		languages: string[];
+		location: string; // location id
+		profession: PROFESSION;
+		status: STATUS;
+	};
+	dm: {
+		immunities: string[];
+		stats: Stats;
+		alignment: string;
+	};
+	draft: boolean; // so i can create them but they wont be seen publically
+}
+
+export interface Stats {
+	ac: number; //armour class
+	speed: number;
+	hp: number; //hit points
+	cr: number; //challenge rating
+
+	con: Stat;
+	str: Stat;
+	dex: Stat;
+	int: Stat;
+	wis: Stat;
+	cha: Stat;
+
+	traits: Additional_Info[];
+	actions: Additional_Info[];
+}
+
+export interface Stat {
+	value: number;
+	modifier: number;
+	save: number;
+}
+
+export interface Additional_Info {
+	title: string;
+	description: string;
+}
+
+export interface Additional_Notes {
+	session: string;
+	discovery: string;
+}
+
+export enum PROFESSION {
+	Merchant = "merchant",
+	Guard = "guard",
+	Noble = "noble",
+	Clergy = "clergy",
+	Criminal = "criminal",
+	Other = "other",
+}
+
+export enum STATUS {
+	Met = "met",
+	Heard_About = "heard_about",
+	Unknown = "unknown",
 }
